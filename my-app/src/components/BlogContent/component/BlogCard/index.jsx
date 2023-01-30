7b551706e336b216c856c911765ee6c895310da7
aleroom <aleroom@yahoo.com>
2023-01-30 17:30:53 +0300

    add

--- my-app/src/components/BlogContent/component/BlogCard/index.jsx
+++ my-app/src/components/BlogContent/component/BlogCard/index.jsx
@@ -1,10 +1,13 @@
 import "./BlogCard.module.css";
 
-const BlogCard = ({title, description}) => {
+const BlogCard = ({title, description, pos}) => {
     return (
         <div className="post">
             <h2>{title}</h2>
             <p>{description}</p>
+            <div>
+                <button onClick={() => alert(pos)}>Like</button>
+            </div>
         </div>
     );
 };
--- my-app/src/components/BlogContent/index.jsx
+++ my-app/src/components/BlogContent/index.jsx
@@ -1,26 +1,39 @@
 import posts from "../common/projectData/index.jsx";
 import BlogCard from "./component/BlogCard/index.jsx";
+import React, {useState} from "react";
 
 const BlogContent = () => { 
 
-    const blogPosts = posts.map ((item) => {
+    const blogPosts = posts.map ((item, pos) => {
         return (
             <BlogCard 
                 key={item.id}
                 title = {item.title}
                 description = {item.description}
+                pos={pos}
             
             />
         )
     })
-    
+    // const [click, setClick] = useState(true);
+
+    // // const handleClick = () => {
+    //     setClick(false)
+    // }
     return (
         <>
-            <button onClick={() => alert ('Работает')}>Скрыть блог</button>
-            <h1>SimpleBlog</h1>
-            <div className="posts">
-                {blogPosts}
-            </div>
+            {/* <button onClick={() => handleClick}>Скрыть блог</button> */}
+            
+              <>
+                <h1>SimpleBlog</h1>
+                <div className="posts">
+                    {blogPosts}
+                </div>  
+            </>  
+            
+        }
+            
+            
 
             
         </>
